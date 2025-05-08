982126490aad095c596b8b14425ba1e9b23b6947
jotape1nn <joaopaulolimamourafju@gmail.com>
2025-05-08 14:00:05 -0300

    for

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("zebra")
+// let numero = Number(prompt(" 1- saque, 2- tranfer, 3- saldo, 4- extrato"));
+
+
+// switch(numero){
+
+//      case 1 :
+//           alert("saque")
+//           break;
+//      case 2 :
+//           alert("transfer")
+//           break;
+//      case 3 :
+//           alert("saldo")
+//           break;
+//      case 4 :
+//           alert("extrato")
+//           break;
+//      default: 
+//           alert("numero iválido")
+// }
+
+
+for(let i = 2; i <= 20; i = i + 2){
+    console.log(`passo ${i}`)
+}
